642eb5e7a1b094bc92bbf727449122c68eb9dffa
rowendev <rowendevtw@gmail.com>
2022-09-22 15:09:21 +0800

    add profile functions

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,91 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { getAuth, updateProfile } from "firebase/auth";
+import { updateDoc, doc } from "firebase/firestore";
+import { db } from "../firebase.config";
+import { toast } from "react-toastify";
 
 function Profile() {
+  const auth = getAuth();
+  const [changeDetails, setChangeDetails] = useState(false);
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
+  const { name, email } = formData;
+  const navigate = useNavigate();
+  const onLogout = () => {
+    auth.signOut();
+    navigate("/");
+  };
+  const onSubmit = async () => {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        // update displayName in firebase
+        // object to put update data
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+        // update in firestore
+        // doc( db, 'collection', params)
+        const userRef = doc(db, "users", auth.currentUser.uid);
+        await updateDoc(userRef, {
+          name,
+        });
+      }
+    } catch (error) {
+      toast.error("Could not update profile");
+    }
+  };
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
   return (
-    <div>
-      <h1>Profile</h1>
+    <div className="profile">
+      <header className="profileHeader">
+        <p className="pageHeader">My Profile</p>
+        <button type="button" className="logOut" onClick={onLogout}>
+          Log out
+        </button>
+      </header>
+
+      <main>
+        <div className="profileDetailsHeader">
+          <p className="profileDetailsText">Personal Details</p>
+          <p
+            className="changePersonalDetails"
+            onClick={() => {
+              changeDetails && onSubmit();
+              setChangeDetails((prevState) => !prevState);
+            }}
+          >
+            {changeDetails ? "done" : "change"}
+          </p>
+        </div>
+        <div className="profileCard">
+          <input
+            type="text"
+            id="name"
+            className={!changeDetails ? "profileName" : "profileNameActive"}
+            disabled={!changeDetails}
+            value={name}
+            onChange={onChange}
+          />
+          <hr />
+          <input
+            type="text"
+            id="email"
+            // className={!changeDetails ? "profileEmail" : "profileEmailActive"}
+            className="profileEmail"
+            // disabled={!changeDetails}
+            value={email}
+            // onChange={onChange}
+          />
+        </div>
+      </main>
     </div>
   );
 }
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
+import { toast } from "react-toastify";
 import { Link, useNavigate } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 
@@ -12,7 +14,28 @@ function SignIn() {
 
   const { email, password } = formData;
   const navigate = useNavigate();
-  const onChange = (e) => {};
+  const onChange = (e) => {
+    setFormData((preState) => ({
+      ...preState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      if (userCredential.user) {
+        navigate("/");
+      }
+    } catch (error) {
+      toast.error("Email & Password not correct.");
+    }
+  };
   return (
     <>
       <div className="pageContainer">
@@ -20,7 +43,7 @@ function SignIn() {
           <p className="pageHeader">Welcome Back!</p>
         </header>
 
-        <form>
+        <form onSubmit={onSubmit}>
           <input
             type="email"
             className="emailInput"
@@ -59,6 +82,9 @@ function SignIn() {
           </div>
         </form>
         {/* google oauth */}
+        <Link to="/sign-up" className="registerLink">
+          Sign Up Instead
+        </Link>
       </div>
     </>
   );
